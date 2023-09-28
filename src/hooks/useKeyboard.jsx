91b8e2dbe656a2aa5bd3353134c0bb71bd1164c7
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-09-28 22:27:12 +0200

    [TASK] create hook to listen event from keyboard

---
+++ src/hooks/useKeyboard.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react'
+
+const ACTIONS_KEYBOARD_MAP = {
+  KeyW: 'moveForward',
+  KeyS: 'moveBackward',
+  KeyA: 'moveLeft',
+  KeyD: 'moveRight',
+  Space: 'jump',
+  Digit1: 'dirt',
+  Digit2: 'grass',
+  Digit3: 'glass',
+  Digit4: 'wood',
+  Digit5: 'log'
+}
+
+export const useKeyboard = () => {
+  const [actions, setActions] = useState(
+    {
+      moveForward: false,
+      moveBackward: false,
+      moveLeft: false,
+      moveRight: false,
+      jump: false,
+      dirt: false,
+      grass: false,
+      glass: false,
+      wood: false,
+      log: false
+    })
+  useEffect(() => {
+    const handleKeyDown = event => {
+      const { code } = event
+      const action = ACTIONS_KEYBOARD_MAP[code]
+      if (action) {
+        // if (actions[action]) return
+        console.log(action)
+        setActions(prevActions => ({
+          ...prevActions,
+          [action]: true
+        }))
+      }
+    }
+
+    const handleKeyUp = event => {
+      const { code } = event
+      const action = ACTIONS_KEYBOARD_MAP[code]
+      if (action) {
+        // if (!actions[action]) return
+        setActions(prevActions => ({
+          ...prevActions,
+          [action]: false
+        }))
+      }
+    }
+
+    const handleKey = (event, modus) => {
+      const { code } = event
+      const action = ACTIONS_KEYBOARD_MAP[code]
+      if (action) {
+        setActions(prevActions => (
+          {
+            ...prevActions,
+            [action]: modus === 'down'
+          }
+        ))
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener('keyup', handleKeyUp)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener('keyup', handleKeyUp)
+    }
+  }, [])
+
+  return actions
+}
